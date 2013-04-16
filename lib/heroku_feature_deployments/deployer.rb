acdc6c0727792881b75e188edfbf49969b1300ee
Matt Beedle <mattbeedle@googlemail.com>
2013-04-16 15:55:13 +0200

    Add SUBDOMAIN to environment vars automatically

--- lib/heroku_feature_deployments/deployer.rb
+++ lib/heroku_feature_deployments/deployer.rb
@@ -95,6 +95,7 @@ module HerokuFeatureDeployments
 
     def add_environment_variables
       config.logger.info "Adding environment variables"
+      config.env_vars.merge!(SUBDOMAIN: @full_app_name)
       heroku.put_config_vars(@full_app_name, config.env_vars)
     end
 
