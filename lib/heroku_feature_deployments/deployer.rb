7cc2d68e5751407db8a9aeed86e552c5319467c7
Matt Beedle <mattbeedle@googlemail.com>
2013-05-16 09:47:41 +0200

    Don't use full_app_name in subdomain env var

--- lib/heroku_feature_deployments/deployer.rb
+++ lib/heroku_feature_deployments/deployer.rb
@@ -116,7 +116,7 @@ module HerokuFeatureDeployments
 
     def add_environment_variables
       config.logger.info "Adding environment variables"
-      config.env_vars.merge!('SUBDOMAIN' => @full_app_name)
+      config.env_vars.merge!('SUBDOMAIN' => @app_name)
       heroku.put_config_vars(@full_app_name, config.env_vars)
     end
 
